d4d7c5f967547cd8b64e9ffa3af6c4fcbae7d984
Samuele Voltan <samu.voltan@gmail.com>
2021-04-08 18:28:38 +0200

    Minor changes

--- lab1/ex01.1/main.cpp
+++ lab1/ex01.1/main.cpp
@@ -19,53 +19,51 @@ using namespace std;
 double error(double, double, int);
 Random init();
 
-int main (int argc, char *argv[]){
+int main() {
 
 	Random rnd;
 	rnd = init();
 
-  	int N_throws = 10000;
-  	int N_blocks = 100;
+  	const int N_throws = 10000;
+  	const int N_blocks = 100;
 
   	ofstream data1 ("ex01.1.1.out"); // <r>
   	ofstream data2 ("ex01.1.2.out"); // sigma2
   	ofstream data3 ("ex01.1.3.out"); // chi2
 
-	// ==== NOTA BENE ====
-	// Le variabili che terminano con "_prog" vengono inizializzate qui sotto e non devono MAI essere toccate all'interno dei cicli	
-
 	double sum_prog = 0., sum2_prog = 0.;
 	double sigma_prog = 0., sigma2_prog = 0.;
-   	for(int i=0; i<N_blocks; i++){
-     	double sum = 0.;
-     	double sigma = 0.;
-     	int chi[N_blocks] = {0};
-     	for(int j=0; j<N_throws; j++){
-       		double ran = rnd.Rannyu();
-       		sum += ran;
+   	
+	for(int i=0; i<N_blocks; i++){
+	     	double sum = 0.;
+		double sigma = 0.;
+		int chi[N_blocks] = {0};
+		for(int j=0; j<N_throws; j++){
+			double ran = rnd.Rannyu();
+			sum += ran;
 			sigma += (ran - 0.5) * (ran - 0.5);
 			chi[(int) (ran*N_blocks)] ++;
-     	}
-	// ==== ANALISI DATI (metodo a blocchi) ====
+		}
+		// ==== ANALISI DATI (metodo a blocchi) ====
 		// <r>
-     	sum_prog += sum / N_throws;
-     	sum2_prog += sum / N_throws * sum / N_throws;
-     	double av = sum_prog / (i + 1);
-     	double av2 = sum2_prog / (i + 1);
-     	data1 << av << "\t" << error(av, av2, i) << endl;
-     	// sigma2
-     	sigma_prog += sigma / N_throws;
-     	sigma2_prog += sigma / N_throws * sigma / N_throws;
-     	av = sigma_prog / (i + 1);
-     	av2 = sigma2_prog / (i + 1);
-     	data2 << av << "\t" << error(av, av2, i) << endl;
-     	// chi2
-     	double chi2 = 0.;
-     	for(int k=0; k<N_blocks; k++) {
-     		chi2 += (chi[k] - N_throws/N_blocks) * (chi[k] - N_throws/N_blocks);
-     	}
-     	chi2 *= ((double) N_blocks / N_throws);
-     	data3 << chi2 << endl;
+		sum_prog += sum / N_throws;
+		sum2_prog += sum / N_throws * sum / N_throws;
+		double av = sum_prog / (i + 1);
+		double av2 = sum2_prog / (i + 1);
+		data1 << av << "\t" << error(av, av2, i) << endl;
+		// sigma2
+		sigma_prog += sigma / N_throws;
+		sigma2_prog += sigma / N_throws * sigma / N_throws;
+		av = sigma_prog / (i + 1);
+		av2 = sigma2_prog / (i + 1);
+		data2 << av << "\t" << error(av, av2, i) << endl;
+		// chi2
+		double chi2 = 0.;
+		for(int k=0; k<N_blocks; k++) {
+			chi2 += (chi[k] - N_throws/N_blocks) * (chi[k] - N_throws/N_blocks);
+		}
+		chi2 *= ((double) N_blocks / N_throws);
+		data3 << chi2 << endl;
 	}	
 
 	rnd.SaveSeed();
@@ -92,7 +90,7 @@ Random init() {
    	Primes.close();
 
    	ifstream input("seed.in");
-   	string property; //D=26
+   	string property; // D = 26
    	if (input.is_open()){
       	while ( !input.eof() ){
          	input >> property;
