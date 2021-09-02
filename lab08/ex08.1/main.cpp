c09b3659be96c092edafb94795c2ef196fa5e1ec
Samuele Voltan <samu.voltan@gmail.com>
2021-09-02 21:34:43 +0200

    Lab08 ready for final check

--- lab08/ex08.1/main.cpp
+++ lab08/ex08.1/main.cpp
@@ -25,36 +25,44 @@ double D2_psi(double, double, double);
 
 int main() {
 
+	// initialize random and output
 	Random rnd;
-	ofstream write_blocks;
+	ofstream write_blocks("data/ex08.1.out");
 
-	double mu = 0.5, sigma = 1.;
+	// initialize delta (already tuned), x and parameters
+	double delta = 2.365;
+	double x = 1.5;
+	const double mu = 0.5, sigma = 1.;
+
+	// defines the function to sample: |psi|^2
 	auto psi_T2 = [mu, sigma](double x[]) {
 		double psi = psi_T(x[0], mu, sigma);
 		return psi * psi;
 	};
 
-	double delta = 2.365; // tuned with Random::Tune
-	double x = 1.5;
-	rnd.Tune(&x, 1, delta, psi_T2, "Uniform");
+	// this should take only few steps since delta is already tuned
+	rnd.Tune<double>(&x, 1, delta, psi_T2, "Uniform");
 
-	write_blocks.open("ex08.1bis.out");
+	// data blocking for the energy
    	for(int i=0; i<N_blocks; i++) {
 		double sum = 0.;
 		for(int j=0; j<N_throws; j++) {
-//			sum += (-0.5 * D2_psi(x) + V(x) * psi_T(x)) / psi_T(x);
+			// energy
 			sum += (-0.5 * D2_psi(x, mu, sigma)) / psi_T(x, mu, sigma) + V(x);
-			rnd.Metropolis(&x, 1, delta, psi_T2, "Uniform");
+			// samples new x
+			rnd.Metropolis<double>(&x, 1, delta, psi_T2, "Uniform");
 		}
 		sum /= N_throws;
 		write_blocks << sum << endl;
 	}
-	write_blocks.close();
 
 	rnd.SaveSeed();
    	return 0;
 }
 
+/*
+ * This is the trial wavefunction psi_T(x)
+ */
 double psi_T(double x, double mu, double sigma) {
 	double phi_p = x + mu;
 	double phi_m = x - mu;
@@ -64,10 +72,16 @@ double psi_T(double x, double mu, double sigma) {
 	return exp(-y * phi_p) + exp(-y * phi_m);
 }
 
+/*
+ * This is the potential V(x)
+ */
 double V(double x) {
 	return x*x*x*x - 2.5 * x*x;
 }
 
+/*
+ * This is the second derivative of the trial wavefunction
+ */
 double D2_psi(double x, double mu, double sigma) {
 	double phi_p = x + mu;
 	double phi_m = x - mu;
--- lab08/ex08.2/main.cpp
+++ lab08/ex08.2/main.cpp
@@ -30,30 +30,47 @@ double D2_psi(double, double, double);
 
 int main(int argc, char *argv[]) {
 
+	// initialize random and output
 	Random rnd;
 	ofstream write;
+
+	// define parameters, initialize x
 	double mu, sigma;
 	double x = 1.5;
-	double delta = 1.72;	// tuned with Random::Tune<double>, now commented, with values of mu_min and sigma_min
-							// note that you require to define Psi_T2 in order to tune again
+
+	// delta has already been tuned: note that
+	// you require to define Psi_T2 in order to tune it again.
+	// This is different from lab08/ex08.1, since now Psi_T2 is
+	// defined every time we iterate over the grid of (mu, sigma)
+	double delta = 1.72;
+
+	// runs the algorithm that finds the minimum of the energy
 	if(string{argv[1]} == "Grid") {
+
+		// less throws are required here
 		const int N_throws = 1000;
 		write.open("data/ex08.2_grid.out");
 		mu = mu_min;
 		sigma = sigma_min;
 		double mu_step = (mu_max - mu_min) / N_grid;
 		double sigma_step = (sigma_max - sigma_min) / N_grid;
+
+		// 2-dimensional grid (mu, sigma)
 		for(int i=0; i<N_grid; ++i) {
 			mu = mu_min + i * mu_step;
 			for(int j=0; j<N_grid; ++j) {
 				sigma = sigma_min + j * sigma_step;
+
+				// data blocking method for value of energy H
 				for(int k=0; k<N_blocks; ++k) {
+
+					//defines function to sample: |psi|^2
 					auto psi_T2 = [mu, sigma](double x[]) {
 						double psi = psi_T(*x, mu, sigma);
 						return psi * psi;
 					};
-					// I thought about tuning every time, in order to maintain acceptance rate. It is simply too expensive
-					//rnd.Tune<double>(&x, 1, delta, psi_T2, "Uniform");
+					// here I thought about tuning every time, in order to
+					// maintain a perfect acceptance rate: it is too expensive
 					double H = 0.;
 					for(int l=0; l<N_throws; ++l) {
 						rnd.Metropolis<double>(&x, 1, delta, psi_T2, "Uniform");
@@ -66,17 +83,29 @@ int main(int argc, char *argv[]) {
 			}
 		}
 	}
+
+	// runs the algorithm that, given the duple (mu, sigma) that
+	// minimizes the energy, calculates it
 	else if(string{argv[1]} == "Sample") {
+
+		// a larger number of throws is needed here
 		const int N_throws = 10000;
 		write.open("data/ex08.2_measure.out");
 		ofstream sample("data/ex08.2_sample.out");
+
+		// reads mu and sigma from input. NOTE that I require to
+		// run the code twice as I carried out the search for the 
+		// minima of H with python in the notebook
 		mu = atof(argv[2]);
 		sigma = atof(argv[3]);
-		cout << mu << "\t" << sigma << endl;
+
+		// defines the function to be sampled
 		auto psi_T2 = [mu, sigma](double x[]) {
 			double psi = psi_T(*x, mu, sigma);
 			return psi * psi;
 		};
+
+		// data blocking for H and also sampling of x from the distribution
 		for(int i=0; i<N_blocks; ++i) {
 			double H = 0.;
 			for(int j=0; j<N_throws; ++j) {
@@ -88,8 +117,11 @@ int main(int argc, char *argv[]) {
 			write << H << endl;
 		}
 	}
+
+	// gives instruction if wrong input
 	else {
-		cerr << "Usage: " << argv[0] << "<Grid/Sample>" << endl;
+		cerr << "Usage: " << argv[0] << " Grid >> if you want to find the minimum of H\n" <<
+		"Otherwise\nUsage: " << argv[0] << " Sample <mu> <sigma> >> if you already know it\n";
 		return -1;
 	}
 
@@ -97,6 +129,9 @@ int main(int argc, char *argv[]) {
    	return 0;
 }
 
+/*
+ * This is the trial wavefunction psi_T(x)
+ */
 double psi_T(double x, double mu, double sigma) {
 	double phi_p = x + mu;
 	double phi_m = x - mu;
@@ -106,10 +141,16 @@ double psi_T(double x, double mu, double sigma) {
 	return exp(-y * phi_p) + exp(-y * phi_m);
 }
 
+/*
+ * This is the potential V(x)
+ */
 double V(double x) {
 	return x*x*x*x - 2.5 * x*x;
 }
 
+/*
+ * This is the second derivative of the trial wavefunction
+ */
 double D2_psi(double x, double mu, double sigma) {
 	double phi_p = (x + mu) / sigma;
 	double phi_m = (x - mu) / sigma;
@@ -118,43 +159,3 @@ double D2_psi(double x, double mu, double sigma) {
 	double y = (phi_m - 1.) * exp(-0.5 * phi_m) + (phi_p - 1.) * exp(-0.5 * phi_p);
 	return y / (sigma * sigma);
 }
-/*
-double Dmu_psi(double x, double mu, double sigma) {
-	double phi_p = (x + mu) / sigma;
-	double phi_m = (x - mu) / sigma;
-	double psi_p = exp(-0.5 * phi_p * phi_p);
-	double psi_m = exp(-0.5 * phi_m * phi_m);
-	return (phi_m*psi_m - phi_p*psi_p) / sigma;
-}
-
-double Dsigma_psi(double x, double mu, double sigma) {
-	double phi_p = (x + mu) / sigma;
-	double phi_m = (x - mu) / sigma;
-	double psi_p = exp(-0.5 * phi_p * phi_p);
-	double psi_m = exp(-0.5 * phi_m * phi_m);
-	return (phi_m*phi_m*psi_m + phi_p*phi_p*psi_p) / sigma;
-}
-
-double Dmu_D2_psi(double x, double mu, double sigma) {
-	double phi_p = (x + mu) / sigma;
-	double phi_m = (x - mu) / sigma;
-	double psi_p = exp(-0.5 * phi_p * phi_p);
-	double psi_m = exp(-0.5 * phi_m * phi_m);
-	return ((-1. - phi_m + phi_m*phi_m)*psi_m + (1. + phi_p - phi_p*phi_p)*psi_p) / sigma;
-}
-
-double Dsigma_D2_psi(double x, double mu, double sigma) {
-	double phi_p = (x + mu) / sigma;
-	double phi_m = (x - mu) / sigma;
-	double psi_p = exp(-0.5 * phi_p * phi_p);
-	double psi_m = exp(-0.5 * phi_m * phi_m);
-	return ((1. - phi_m + phi_m*phi_m)*psi_m*phi_m + (1. - phi_p + phi_p*phi_p)*psi_p*phi_p) / sigma;
-}
-
-double Dmu_H(double x, double mu, double sigma) {
-	return Dmu_D2_psi(x, mu, sigma) / psi_T(x, mu, sigma) - (D2_psi(x, mu, sigma)*Dmu_psi(x, mu, sigma)) / (psi_T(x, mu, sigma)*psi_T(x, mu, sigma));
-}
-
-double Dsigma_H(double x, double mu, double sigma) {
-	return Dsigma_D2_psi(x, mu, sigma) / psi_T(x, mu, sigma) - (D2_psi(x, mu, sigma)*Dsigma_psi(x, mu, sigma)) / (psi_T(x, mu, sigma)*psi_T(x, mu, sigma));
-}*/
